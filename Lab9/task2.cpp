91e501898de23acaa626ea281288d5555efc8442
Muhammad Talha <muhammadtalha895c@gmail.com>
2025-11-14 15:18:21 +0500

    TASK 2 COMPLETEDD!!!

---
+++ Lab9/task2.cpp
@@ -0,0 +1,148 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    Node *left;
+    Node *right;
+    int height;
+
+    Node(int data) {
+        key = data;
+        left = right = nullptr;
+        height = 0; 
+    }
+};
+
+class AVL {
+public:
+    Node* root;
+
+    AVL() {
+        root = nullptr;
+    }
+
+    int height(Node* node) {
+        if (node == nullptr) {
+            return -1;
+        }
+        return node->height;
+    }
+
+  
+    int getBalance(Node* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        return height(node->left) - height(node->right);
+    }
+
+   
+    Node* rightRotate(Node* y) {
+        Node* x = y->left;
+        Node* T2 = x->right;
+
+      
+        x->right = y;
+        y->left = T2;
+
+      
+        y->height = max(height(y->left), height(y->right)) + 1;
+        x->height = max(height(x->left), height(x->right)) + 1;
+
+        return x;
+    }
+
+    Node* leftRotate(Node* x) {
+        Node* y = x->right;
+        Node* T2 = y->left;
+
+     
+        y->left = x;
+        x->right = T2;
+
+      
+        x->height = max(height(x->left), height(x->right)) + 1;
+        y->height = max(height(y->left), height(y->right)) + 1;
+
+        
+        return y;
+    }
+
+    Node* insert(Node* node, int key) {
+      
+        if (node == nullptr) {
+            return new Node(key);
+        }
+
+        if (key < node->key) {
+            node->left = insert(node->left, key);
+        } else if (key > node->key) {
+            node->right = insert(node->right, key);
+        } else {
+            return node;  
+        }
+
+        node->height = max(height(node->left), height(node->right)) + 1;
+
+        
+        int balance = getBalance(node);
+
+        if (balance > 1 && key < node->left->key) {
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && key > node->right->key) {
+            return leftRotate(node);
+        }
+
+        if (balance > 1 && key > node->left->key) {
+            node->left = leftRotate(node->left);
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && key < node->right->key) {
+            node->right = rightRotate(node->right);
+            return leftRotate(node);
+        }
+
+   
+        return node;
+    }
+    
+
+    void inorder(Node* root) {
+        if (root != nullptr) {
+            inorder(root->left);
+            cout << root->key << " ";
+            inorder(root->right);
+        }
+    }
+};
+
+int main() {
+    AVL tree;
+
+    
+    tree.root = tree.insert(tree.root, 50);
+    tree.root = tree.insert(tree.root, 30);
+    tree.root = tree.insert(tree.root, 70);
+    tree.root = tree.insert(tree.root, 20);
+    tree.root = tree.insert(tree.root, 40);
+    tree.root = tree.insert(tree.root, 60);
+    tree.root = tree.insert(tree.root, 80);
+
+    cout << "Inorder before inserting 55: ";
+    tree.inorder(tree.root);
+    cout << endl;
+
+    tree.root = tree.insert(tree.root, 55);
+  tree.root= tree.leftRotate(tree.root);
+     cout << "Inorder after inserting 55 & left rotation: ";
+
+    tree.inorder(tree.root);
+    cout << endl;
+
+    return 0;
+}
